91df8ea0b61234a599770477f03ca7ae4eeb4f59
Jarrett Markman <97311712+jarrett-markman@users.noreply.github.com>
2024-02-29 16:43:25 -0500

    Update app.R

--- app.R
+++ app.R
@@ -1,13 +1,17 @@
 # Player shiny app for atp/wta players performances w line plots for wins & losses
+# Libraries
 library(tidyverse)
 library(shiny)
+# Read in data and create a variable for league
 atp <- read_csv("atp_matches_2023.csv") %>%
   mutate(league = "ATP")
 wta <- read_csv("wta_matches_2023.csv") %>%
   mutate(league = "WTA")
+# Combine df's for aggregating
 data <- bind_rows(atp, wta)
+# Get performance data based on columns for winners
 winners <- data %>%
-  select(
+  select( # Select and rename certain columns
     tourney_id, tourney_level, tourney_date, tourney_name,
     player_id = winner_id,
     seed = winner_seed,
@@ -46,10 +50,10 @@ winners <- data %>%
     league, surface
   ) %>%
   mutate(
-    result = "win"
+    result = "win" # Create a column for "win" - important later
   )
 losers <- data %>%
-  select(
+  select( # Repeat process for the losers
     tourney_id, tourney_level, tourney_date, tourney_name,
     opp_hand = winner_hand,
     opp_height = winner_ht,
@@ -88,15 +92,16 @@ losers <- data %>%
     league, surface
   ) %>%
   mutate(
-    result = "loss"
+    result = "loss" # Create a result = loss column for losers - important later
   )
+# Combine winners and losers into one data frame
 df <- bind_rows(winners, losers)
-df %>% filter(tourney_level == "P")
 plot <- df %>%
-  filter(!is.na(first_serve_points)) %>%
-  group_by(name, result, league, tourney_id, tourney_date) %>%
-  summarise(
-    tourney_id, surface,
+  na.omit() %>% # Remove all NAs
+  group_by(name, result, league, tourney_id, tourney_date) %>% # Group by player, match result, league and each tournament 
+  summarise( # Summarise columns in the data set
+    tourney_id, surface, 
+    # Change the tourney_level column based on the level of tournament e.g., G -> Grand Slams
     tourney_level = ifelse(tourney_level == "G", "Grand Slams", tourney_level),
     tourney_level = ifelse(tourney_level == "M", "Masters 1000s", tourney_level),
     tourney_level = ifelse(tourney_level == "C", "Challengers", tourney_level),
@@ -112,7 +117,8 @@ plot <- df %>%
     "Opponent Age" = opp_age,
     "Opponent Height" = opp_height,
     Aces = aces,
-    DFs = dfs,
+    DFs = dfs, 
+    # Calculate player rate stats
     "First Serve In Percentage" = (first_in/first_serve_points) * 100,
     "First Serve Win Percentage" = (first_won/first_in) * 100,
     "Second Serve In Percentage" = ((first_serve_points - first_in - dfs)/(first_serve_points - first_in)) * 100,    
@@ -128,12 +134,14 @@ plot <- df %>%
   ungroup() %>%
   select(-tourney_date) %>%
   mutate(match_date = paste0(month, day, year), 
-         match_date = as.Date(lubridate::mdy(match_date)))
+         match_date = as.Date(lubridate::mdy(match_date))) # Create a column that stores the data as a mdy object
+# Order the data frame based on the time of year (Jan - Dec)
 plot <- plot[order(plot$match_date, decreasing = TRUE), ] %>%
+# Rename vars
   mutate("Opponent Rank" = as.numeric(opp_rank),
-         Result = ifelse(result == "win", "Win", "Loss")) %>%
-  na.omit() %>%
-  select(-c(result, opp_rank, tourney_id, day))
+         Result = ifelse(result == "win", "Win", "Loss")) %>% 
+  select(-c(result, opp_rank, tourney_id, day)) # Drop unimportant columns
+# Change up variable names in the data frame
 colnames(plot) <- c("Player", "League", "Surface", "Tournament Type", "Tournament", "Opponent Age",
                     "Opponent Height", "Aces", "Double Faults", 
                     "First Serve In Percentage", "First Serve Win Percentage",
@@ -141,6 +149,7 @@ colnames(plot) <- c("Player", "League", "Surface", "Tournament Type", "Tournamen
                     "First Serve Return Win Percentage", "Second Serve Return Win Percentage",
                     "Break Points Converted Percentage", "Year", "Month", "match_date", "Opponent Rank", "Result"
                   )
+# Arrange players in alphabetic order and filter for each league (ATP & WTA)
 atp_plot <- plot %>%
   filter(League == "ATP") %>%
   select(-League) %>% 
@@ -149,29 +158,34 @@ wta_plot <- plot %>%
   filter(League == "WTA") %>%
   select(-League) %>% 
   arrange(Player)
-# Create shiny ui and server
+# Create shiny app
 ui <- fluidPage(
+  # Title shinyApp
   titlePanel("Evaluating Statistical Performance in Wins and Losses in the ATP and WTA"),
   tabsetPanel(
     id = "mainTab",
+    # Create a home page that displays and image for atp and wta
+    # Order the images set them off to the left/right
     tabPanel(
       "Home Page",
       fluidRow(
         column(6,
-               actionLink("atp_link", 
+               actionLink("atp_link", # Hyperink the object
                           tags$img(src="https://upload.wikimedia.org/wikipedia/en/thumb/3/3f/ATP_Tour_logo.svg/1200px-ATP_Tour_logo.svg.png", 
                                    style="max-width: 100%; max-height: 100%; margin-top: 50px;"))
         ),
         column(6,
-               actionLink("wta_link", 
+               actionLink("wta_link", # Hyperlink the object
                           tags$img(src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5f/Women%27s_Tennis_Association_logo_%282020%29.svg/1200px-Women%27s_Tennis_Association_logo_%282020%29.svg.png", 
                                    style="max-width: 100%; max-height: 100%; margin-top: 50px;"))
       )
     )
   ) ,
+  # Create an ATP tabPanel
   tabPanel(
     "ATP",
     id = "ATP",
+    # Create inputs for graphs (player, stat, filters)
     sidebarPanel(
       selectInput("player_atp", "Player: ", choices = unique(atp_plot$Player)),
       selectInput("stat_atp", "Stat: ", choices = colnames(atp_plot[-c(1:4, 17, 19, 21)])),
@@ -180,9 +194,10 @@ ui <- fluidPage(
       actionButton("submit_atp", "Submit")
     ),
     mainPanel(
-      plotOutput("plot_atp")
+      plotOutput("plot_atp") # Display the plot
     )
   ),
+    # Repeat inputs for WTA
   tabPanel(
     "WTA",
     id = "WTA",
@@ -191,7 +206,6 @@ ui <- fluidPage(
       selectInput("stat_wta", "Stat: ", choices = colnames(wta_plot[-c(1:4, 17, 19, 21)])),
       checkboxGroupInput("tournament_wta", "Tournament Type:", choices = unique(wta_plot$`Tournament Type`), selected = unique(wta_plot$`Tournament Type`)),
       checkboxGroupInput("surface_wta", "Surface: ", choices = unique(wta_plot$Surface), selected = unique(wta_plot$Surface)),
-      # add filter inputs
       actionButton("submit_wta", "Submit")
     ),
     mainPanel(
@@ -201,14 +215,16 @@ ui <- fluidPage(
   tabPanel(
     "Sources",
     id = "Sources",
+    # Cited source
     h1("All the data used is collected from ", 
        tags$a("Jeff Sackman's GitHub", href = "https://github.com/JeffSackmann"), 
        ".")
   )
 ))
-
+# Create server
 server <- function(input, output, session) {
-  
+
+  # Observe the event the atp/wta photo is clicked
   observeEvent(input$atp_link, {
     updateTabsetPanel(session, "mainTab", selected = "ATP")
   })
@@ -216,24 +232,30 @@ server <- function(input, output, session) {
   observeEvent(input$wta_link, {
     updateTabsetPanel(session, "mainTab", selected = "WTA")
   })
-  
+
+  # Set df's as reactiveVal's
   atp_plot <- reactiveVal(atp_plot)
   wta_plot <- reactiveVal(wta_plot)
   
-  observeEvent(input$submit_atp, {
+  observeEvent(input$submit_atp, { # If the submit button is pressed
     
     plot <- atp_plot() %>%
+    # Filter the user inputs
       filter(Player == input$player_atp &
                `Tournament Type` %in% input$tournament_atp &
                Surface %in% input$surface_atp) %>%
-      ggplot(aes(x = match_date, y = .data[[input$stat_atp]], color = Result)) +
-      geom_point(aes(size = `Opponent Rank`)) +
+    # Make a line and dot plot
+      ggplot(aes(x = match_date, y = .data[[input$stat_atp]] , color = Result)) + # Get the stat input & scale color of line via result
+      geom_point(aes(size = `Opponent Rank`)) + # Scale the dots based on opponent's atp/wta rank
       geom_line() +
       labs(
         x = "",
         title = glue::glue("{input$player_atp} {gsub('`', '', input$stat_atp)} in Wins and Losses Over the 2023 Season")
+        # Use glue to create a title that displays the player and the stat chose
+        # gsub command removes the ` from the display
       ) +
-      scale_colour_manual(values = c("red2", "green2")) +
+      scale_colour_manual(values = c("red2", "green2")) + # Change color scale
+    # Edit plot display and backgroun
       theme_bw() +
       theme(
         plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
@@ -242,11 +264,13 @@ server <- function(input, output, session) {
       )
     
     output$plot_atp <- renderPlot({
-      plot
+      plot # Display plot
     })
   })
-  
+
+  # Repeat observeEvent for atp with wta tab
   observeEvent(input$submit_wta, {
+    
     plot <- wta_plot() %>%
       filter(Player == input$player_wta &
                `Tournament Type` %in% input$tournament_wta &
@@ -262,7 +286,7 @@ server <- function(input, output, session) {
       theme_bw() +
       theme(
         plot.title = element_text(size = 12, face = "bold", hjust = 0.5),
-        panel.background = element_rect(fill = "#7814FF"),
+        panel.background = element_rect(fill = "#7814FF"), # Set hex code for wta logo as background
         axis.ticks.x = element_blank(), axis.title.x = element_blank(), axis.text.x = element_blank()
       )
     
@@ -272,4 +296,4 @@ server <- function(input, output, session) {
   })
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server) # Display the shinyApp
